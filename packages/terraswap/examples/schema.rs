5ce6593517cbdeb9489abc98aa99358e5acd03f3
Kerber0x <kerber0x@protonmail.com>
2022-07-12 11:23:55 +0100

    Regenerate schemas

--- packages/terraswap/examples/schema.rs
+++ packages/terraswap/examples/schema.rs
@@ -3,8 +3,7 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use terraswap::asset::{Asset, AssetInfo, PairInfo};
-use terraswap::pair::{FeatureToggle, PoolFee};
+use terraswap::asset::{Asset, AssetInfo, AssetInfoRaw, AssetRaw, PairInfo, PairInfoRaw};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -12,9 +11,10 @@ fn main() {
     create_dir_all(&out_dir).unwrap();
     remove_schemas(&out_dir).unwrap();
 
-    export_schema(&schema_for!(PairInfo), &out_dir);
     export_schema(&schema_for!(Asset), &out_dir);
     export_schema(&schema_for!(AssetInfo), &out_dir);
-    export_schema(&schema_for!(FeatureToggle), &out_dir);
-    export_schema(&schema_for!(PoolFee), &out_dir);
+    export_schema(&schema_for!(AssetRaw), &out_dir);
+    export_schema(&schema_for!(AssetInfoRaw), &out_dir);
+    export_schema(&schema_for!(PairInfo), &out_dir);
+    export_schema(&schema_for!(PairInfoRaw), &out_dir);
 }
