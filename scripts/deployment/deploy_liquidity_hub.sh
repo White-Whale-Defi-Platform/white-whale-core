98318c730120296f34c433b52f0cac330c022b5c
Kerber0x <kerber0x@protonmail.com>
2022-07-28 13:56:35 +0100

    build: add env variables for terra

--- scripts/deployment/deploy_liquidity_hub.sh
+++ scripts/deployment/deploy_liquidity_hub.sh
@@ -1,13 +1,73 @@
 #!/bin/bash
 set -e
 
+display_usage() {
+  echo "CosmWasm Contract Deployer"
+  echo -e "\nUsage:\./deploy_liquidity_hub.sh [flags]\n"
+  echo -e "Available flags:\n"
+  echo -e "  -h \thelp"
+  echo -e "  -c \tThe chain where you want to deploy (juno|juno-testnet|terra|terra-testnet)"
+}
+
+if [ -z $1 ]; then
+  display_usage
+  exit 0
+fi
+
+# get args
+optstring=':c:h'
+while getopts $optstring arg; do
+  case "$arg" in
+  c)
+    chain=$OPTARG
+    ;;
+  h)
+    display_usage
+    exit 0
+    ;;
+  :)
+    echo "Must supply an argument to -$OPTARG" >&2
+    exit 1
+    ;;
+  ?)
+    echo "Invalid option: -${OPTARG}"
+    display_usage
+    exit 2
+    ;;
+  esac
+done
+
 projectRootPath=$(realpath "$0" | sed 's|\(.*\)/.*|\1|' | cd ../ | pwd)
 
-source <(cat "$projectRootPath"/scripts/deployment/deploy_env/testnets/juno.env)
-deployer='ww_deployer_wallet'
+case $chain in
 
+juno)
+  source <(cat "$projectRootPath"/scripts/deployment/deploy_env/mainnets/juno.env)
+  ;;
+
+juno-testnet)
+  source <(cat "$projectRootPath"/scripts/deployment/deploy_env/testnets/juno.env)
+  ;;
+
+terra)
+  source <(cat "$projectRootPath"/scripts/deployment/deploy_env/mainnets/terra.env)
+  ;;
+
+terra-testnet)
+  source <(cat "$projectRootPath"/scripts/deployment/deploy_env/testnets/terra.env)
+  ;;
+
+*)
+  echo "Network $chain not defined"
+  exit 1
+  ;;
+esac
+
+source <(cat "$projectRootPath"/scripts/deployment/deploy_env/base.env)
+
+deployer='deployer_wallet'
 # import the deployer wallet
-export mnemonic=$(cat "$projectRootPath"/scripts/deployment/deploy_env/mnemonics/juno_deployer_mnemonic.txt)
+export mnemonic=$(cat "$projectRootPath"/scripts/deployment/deploy_env/mnemonics/deployer_mnemonic.txt)
 
 # verify if the deployer wallet has already been imported
 if ! $BINARY keys show $deployer >/dev/null 2>&1; then
@@ -15,11 +75,15 @@ if ! $BINARY keys show $deployer >/dev/null 2>&1; then
   echo "Importing $deployer into $BINARY..."
   echo $mnemonic | $BINARY keys add $deployer --recover >/dev/null 2>&1
 fi
+deployer_address=$($BINARY keys show $deployer --output json | jq -r '.address')
 
 contracts_storage_output='{"contracts": []}'
+
+mkdir -p "$projectRootPath"/scripts/deployment/output
 output_path="$projectRootPath"/scripts/deployment/output/"$CHAIN_ID"_liquidity_hub_contracts.json
 
 # Store all artifacts on chain
+date=$(date -u +"%Y-%m-%dT%H:%M:%S%z")
 for artifact in "$projectRootPath"/artifacts/*.wasm; do
   echo "Storing $(basename $artifact) on $CHAIN_ID..."
   res=$($BINARY tx wasm store $artifact $TXFLAG --from $deployer)
@@ -27,19 +91,19 @@ for artifact in "$projectRootPath"/artifacts/*.wasm; do
   contracts_storage_output=$(echo $contracts_storage_output | jq --arg artifact "$(basename "$artifact")" --arg code_id "$code_id" '.contracts[.contracts|length] |= . + {wasm: $artifact, code_id: $code_id}')
 
   # Download the wasm binary from the chain and compare it to the original one
-  $BINARY query wasm code $code_id --node $RPC downloaded_wasm.wasm
+  echo -e "Verifying integrity of wasm artifact on chain...\n"
+  $BINARY query wasm code $code_id --node $RPC downloaded_wasm.wasm >/dev/null 2>&1
   # The two binaries should be identical
   diff $artifact downloaded_wasm.wasm
   rm downloaded_wasm.wasm
 done
 
 echo $contracts_storage_output | jq '.' >$output_path
-echo "Stored artifacts on $CHAIN_ID successfully!"
+echo -e "\n**** Stored artifacts on $CHAIN_ID successfully ****\n"
 
-echo "Initializing the Liquidity Hub on $CHAIN_ID..."
-deployer_address=$($BINARY keys show $deployer --output json | jq -r '.address')
+echo -e "\nInitializing the Liquidity Hub on $CHAIN_ID..."
 
-echo "Initializing the Fee Collector..."
+echo -e "\nInitializing the Fee Collector..."
 
 # Prepare the instantiation message
 init='{}'
@@ -55,7 +119,7 @@ tmpfile=$(mktemp)
 jq -r --arg contract_address $contract_address '.contracts[] | select (.wasm == "fee_collector.wasm") |= . + {contract_address: $contract_address}' $output_path | jq -n '.contracts |= [inputs]' >$tmpfile
 mv $tmpfile $output_path
 
-echo "Initializing the Pool Factory..."
+echo -e "\nInitializing the Pool Factory..."
 
 # Prepare the instantiation message
 pair_code_id=$(jq -r '.contracts[] | select (.wasm == "terraswap_pair.wasm") | .code_id' $output_path)
@@ -76,7 +140,7 @@ tmpfile=$(mktemp)
 jq -r --arg contract_address $contract_address '.contracts[] | select (.wasm == "terraswap_factory.wasm") |= . + {contract_address: $contract_address}' $output_path | jq -n '.contracts |= [inputs]' >$tmpfile
 mv $tmpfile $output_path
 
-echo "Initializing the Router..."
+echo -e "\nInitializing the Router..."
 
 # Prepare the instantiation message
 terraswap_factory=$(jq '.contracts[] | select (.wasm == "terraswap_factory.wasm") | .contract_address' $output_path)
@@ -94,5 +158,9 @@ tmpfile=$(mktemp)
 jq -r --arg contract_address $contract_address '.contracts[] | select (.wasm == "terraswap_router.wasm") |= . + {contract_address: $contract_address}' $output_path | jq -n '.contracts |= [inputs]' >$tmpfile
 mv $tmpfile $output_path
 
-echo "Deployment successful!"
+tmpfile=$(mktemp)
+jq --arg date "$date" --arg chain_id "$CHAIN_ID" --arg deployer_address "$deployer_address" '. + {date: $date ,chain_id: $chain_id, deployer_address: $deployer_address}' $output_path >$tmpfile
+mv $tmpfile $output_path
+
+echo -e "\n**** Deployment successful ****\n"
 jq '.' $output_path
