015f1477e01875c61e23081e5f75af65819bfb60
Kerber0x <kerber0x@protonmail.com>
2022-09-05 11:20:01 +0100

    fix: fee_collector schemas generation

--- scripts/build_schemas.sh
+++ scripts/build_schemas.sh
@@ -6,11 +6,11 @@ projectRootPath=$(realpath "$0" | sed 's|\(.*\)/.*|\1|' | cd ../ | pwd)
 # Generates schemas for contracts in the liquidity_hub
 for component in "$projectRootPath"/contracts/liquidity_hub/*/; do
   echo "Generating schemas for $(basename $component)..."
-  if [[ "$(basename $component)" == "fee-collector" ]]; then
-    cd $component && cargo schema
+  if [[ "$(basename $component)" == "fee_collector" ]]; then
+    cd $component && cargo schema --locked
   else
     for contract in "$component"*/; do
-      cd $contract && cargo schema
+      cd $contract && cargo schema --locked
     done
   fi
 done
