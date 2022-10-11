855878de2c02e5e89045db868f1c2c857db211c3
Kerber0x <kerber0x@protonmail.com>
2022-10-11 14:30:53 +0100

    refactor(terraswap_pair): improve contract migration, refactor tests

--- terraswap_pair/src/error.rs
+++ terraswap_pair/src/error.rs
@@ -1,10 +1,14 @@
 use cosmwasm_std::{OverflowError, StdError};
+use semver::Version;
 use thiserror::Error;
 
 #[derive(Error, Debug, PartialEq)]
 pub enum ContractError {
     #[error("{0}")]
     Std(#[from] StdError),
+
+    #[error("Semver parsing error: {0}")]
+    SemVer(String),
 
     #[error("{0}")]
     OverflowError(#[from] OverflowError),
@@ -29,4 +33,16 @@ pub enum ContractError {
 
     #[error("Operation disabled, {0}")]
     OperationDisabled(String),
+
+    #[error("Attempt to migrate to version {new_version}, but contract is on a higher version {current_version}")]
+    MigrateInvalidVersion {
+        new_version: Version,
+        current_version: Version,
+    },
+}
+
+impl From<semver::Error> for ContractError {
+    fn from(err: semver::Error) -> Self {
+        Self::SemVer(err.to_string())
+    }
 }
