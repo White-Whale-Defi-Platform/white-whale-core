2be23567a8777bc1049b30b53ae528baf6d58675
Kerber0x <kerber0x@protonmail.com>
2022-10-27 14:34:17 +0100

    chore: print size of wasm files when checking size

--- scripts/check_artifacts_size.sh
+++ scripts/check_artifacts_size.sh
@@ -9,12 +9,13 @@ else
   maximumSize=$1
 fi
 
-echo -e "\nChecking generated wasm artifacts file size..."
+echo -e "\nChecking generated wasm artifacts file size...\n"
 for artifact in artifacts/*.wasm; do
   artifactSize=$(du -k "$artifact" | cut -f 1)
+  echo "$(basename $artifact): $artifactSize kB"
   if [ "$artifactSize" -gt $maximumSize ]; then
     echo "Artifact $(basename $artifact) file size exceeded. Found $artifactSize kB, maximum $maximumSize kB"
     exit 1
   fi
 done
-echo -e "All good!\n"
+echo -e "\nAll good!\n"
