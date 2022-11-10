f6b8927f0f596086b882d281d613472f39198220
kaimen-sano <kaimen_sano@protonmail.com>
2022-11-10 22:15:18 +1300

    fix(vault): Fix deposit LP calculation
    Related to #1 found in the SCV audit done.
    
    Currently, the following scenario incorrectly incentivises actors to
    continuously deposit and withdraw to exploit the profit held in the
    vault contract.
    If Alice deposits into the vault ($10,000 uluna, and receives 10,000 LP
    tokens in return) and the vault experiences profits up to $15,000 uluna,
    then the next depositor (depositing $5,000 uluna) will receive 5,000 LP
    tokens which are worth more than proportional to their deposit amount.
    
    Currently, the second depositor would be able to withdraw $6,666
    (greater than their $5,000 deposit) despite not being an early holder of
    the LP token before the vault inflated in value.
    
    This commit correctly patches this behavior so that Alice is rewarded
    with the $5,000 profits, while Bob is only able to withdraw their
    initial $5,000 deposit.

--- contracts/liquidity_hub/vault-network/vault/src/execute/deposit.rs
+++ contracts/liquidity_hub/vault-network/vault/src/execute/deposit.rs
@@ -1,10 +1,10 @@
 use cosmwasm_std::{to_binary, CosmosMsg, DepsMut, Env, MessageInfo, Response, Uint128, WasmMsg};
 use cw20::{AllowanceResponse, Cw20ExecuteMsg};
-use terraswap::asset::AssetInfo;
+use terraswap::{asset::AssetInfo, querier::query_token_info};
 
 use crate::{
     error::{StdResult, VaultError},
-    state::{CONFIG, LOAN_COUNTER},
+    state::{COLLECTED_PROTOCOL_FEES, CONFIG, LOAN_COUNTER},
 };
 
 pub fn deposit(deps: DepsMut, env: Env, info: MessageInfo, amount: Uint128) -> StdResult<Response> {
@@ -50,13 +50,13 @@ pub fn deposit(deps: DepsMut, env: Env, info: MessageInfo, amount: Uint128) -> S
 
     let mut messages: Vec<CosmosMsg> = vec![];
     // add cw20 transfer message if needed
-    if let AssetInfo::Token { contract_addr } = config.asset_info {
+    if let AssetInfo::Token { contract_addr } = config.asset_info.clone() {
         messages.push(
             WasmMsg::Execute {
                 contract_addr,
                 msg: to_binary(&Cw20ExecuteMsg::TransferFrom {
                     owner: info.sender.clone().into_string(),
-                    recipient: env.contract.address.into_string(),
+                    recipient: env.contract.address.clone().into_string(),
                     amount,
                 })?,
                 funds: vec![],
@@ -66,12 +66,31 @@ pub fn deposit(deps: DepsMut, env: Env, info: MessageInfo, amount: Uint128) -> S
     }
 
     // mint LP token for the sender
+    let total_lp_share =
+        query_token_info(&deps.querier, config.liquidity_token.clone())?.total_supply;
+
+    let lp_amount = if total_lp_share.is_zero() {
+        // first depositor to the vault, mint LP tokens 1:1
+        amount
+    } else {
+        // return based on a share of the total pool
+        let collected_protocol_fees = COLLECTED_PROTOCOL_FEES.load(deps.storage)?;
+        let total_deposits = config
+            .asset_info
+            .query_pool(&deps.querier, deps.api, env.contract.address)?
+            .checked_sub(collected_protocol_fees.amount)?;
+
+        amount
+            .checked_mul(total_lp_share)?
+            .checked_div(total_deposits)?
+    };
+
     messages.push(
         WasmMsg::Execute {
             contract_addr: config.liquidity_token.into_string(),
             msg: to_binary(&Cw20ExecuteMsg::Mint {
                 recipient: info.sender.into_string(),
-                amount,
+                amount: lp_amount,
             })?,
             funds: vec![],
         }
@@ -88,38 +107,50 @@ mod test {
     use cosmwasm_std::{
         coins,
         testing::{mock_dependencies, mock_env, mock_info},
-        to_binary, Addr, Response, StdError, Uint128, WasmMsg,
+        to_binary, Addr, Response, Uint128, WasmMsg,
     };
     use cw20::Cw20ExecuteMsg;
-    use terraswap::asset::AssetInfo;
+    use terraswap::asset::{Asset, AssetInfo};
     use vault_network::vault::Config;
 
     use crate::{
         contract::execute,
         error::VaultError,
-        state::{CONFIG, LOAN_COUNTER},
-        tests::{
-            get_fees, mock_creator, mock_dependencies_lp, mock_execute,
-            mock_instantiate::mock_instantiate,
-        },
+        execute::deposit,
+        state::{COLLECTED_PROTOCOL_FEES, CONFIG, LOAN_COUNTER},
+        tests::{get_fees, mock_creator, mock_dependencies_lp, mock_execute},
     };
 
     #[test]
     fn can_deposit_native() {
-        let (mut deps, env) = mock_instantiate(
-            1,
-            AssetInfo::NativeToken {
-                denom: "uluna".to_string(),
-            },
+        let env = mock_env();
+        let mut deps = mock_dependencies_lp(
+            &[],
+            &[],
+            vec![(
+                "creator".to_string(),
+                env.contract.address.clone().into_string(),
+                Uint128::new(5_000),
+            )],
         );
 
         // inject lp token address to config
         CONFIG
-            .update::<_, StdError>(&mut deps.storage, |mut c| {
-                c.liquidity_token = Addr::unchecked("lp_token");
-
-                Ok(c)
-            })
+            .save(
+                &mut deps.storage,
+                &Config {
+                    owner: mock_creator().sender,
+                    liquidity_token: Addr::unchecked("lp_token"),
+                    asset_info: AssetInfo::NativeToken {
+                        denom: "uluna".to_string(),
+                    },
+                    deposit_enabled: true,
+                    flash_loan_enabled: true,
+                    withdraw_enabled: true,
+                    fee_collector_addr: Addr::unchecked("fee_collector"),
+                    fees: get_fees(),
+                },
+            )
             .unwrap();
 
         // inject loan counter
@@ -147,7 +178,7 @@ mod test {
                     })
                     .unwrap()
                 })
-        )
+        );
     }
 
     #[test]
@@ -363,5 +394,88 @@ mod test {
         );
 
         assert_eq!(res.unwrap_err(), VaultError::DepositDuringLoan {});
+    }
+
+    #[test]
+    fn does_not_dilute_early_holders() {
+        let env = mock_env();
+        // simulate a vault with first depositor having 10,000 LP tokens
+        // and vault having 15,000 of asset
+        // the next depositor should not deposit at a 1:1 rate for asset:LP tokens
+        // otherwise, the earlier depositor will be diluted.
+        let second_depositor = Addr::unchecked("depositor2");
+        let mut deps = mock_dependencies_lp(
+            &[
+                (env.contract.address.as_ref(), &coins(15_000, "uluna")),
+                (second_depositor.as_ref(), &coins(5_000, "uluna")),
+            ],
+            &[(
+                "creator".to_string(),
+                &[("lp_token".to_string(), Uint128::new(10_000))],
+            )],
+            vec![],
+        );
+
+        // inject lp token address to config
+        CONFIG
+            .save(
+                &mut deps.storage,
+                &Config {
+                    owner: mock_creator().sender,
+                    liquidity_token: Addr::unchecked("lp_token"),
+                    asset_info: AssetInfo::NativeToken {
+                        denom: "uluna".to_string(),
+                    },
+                    deposit_enabled: true,
+                    flash_loan_enabled: true,
+                    withdraw_enabled: true,
+                    fee_collector_addr: Addr::unchecked("fee_collector"),
+                    fees: get_fees(),
+                },
+            )
+            .unwrap();
+
+        // inject loan counter
+        LOAN_COUNTER.save(&mut deps.storage, &0).unwrap();
+
+        // set protocol fee to 500 token
+        COLLECTED_PROTOCOL_FEES
+            .save(
+                &mut deps.storage,
+                &Asset {
+                    info: AssetInfo::NativeToken {
+                        denom: "uluna".to_string(),
+                    },
+                    amount: Uint128::new(500),
+                },
+            )
+            .unwrap();
+
+        let res = deposit(
+            deps.as_mut(),
+            env,
+            mock_info(second_depositor.as_str(), &coins(5_000, "uluna")),
+            Uint128::new(5_000),
+        );
+
+        // creator has 10,000 of the LP token in a 14,500 pool
+        // depositor2 should therefore get (5000 / 14500) * 10000 = 3,448 LP tokens
+        // this leaves creator with 10,000 / 13,448 of the total LP supply or 14,500 tokens
+        // depositor2 is entitled to 3,448 / 13,448 of the total LP supply or 4,999 tokens
+
+        assert_eq!(
+            res.unwrap(),
+            Response::new()
+                .add_attributes(vec![("method", "deposit"), ("amount", "5000")])
+                .add_message(WasmMsg::Execute {
+                    contract_addr: "lp_token".to_string(),
+                    funds: vec![],
+                    msg: to_binary(&Cw20ExecuteMsg::Mint {
+                        recipient: second_depositor.to_string(),
+                        amount: Uint128::new(3_448)
+                    })
+                    .unwrap()
+                })
+        );
     }
 }
