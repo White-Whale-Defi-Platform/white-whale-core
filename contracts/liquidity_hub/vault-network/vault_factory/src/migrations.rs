76818517d9d7bbe29eb39bbe94e2b0aeb674d517
Kerber0x <kerber0x@protonmail.com>
2023-01-09 16:13:59 +0000

    fix: fix vault factory migration to v1.1.0

--- contracts/liquidity_hub/vault-network/vault_factory/src/migrations.rs
+++ contracts/liquidity_hub/vault-network/vault_factory/src/migrations.rs
@@ -1,8 +1,9 @@
 use cosmwasm_std::{Addr, DepsMut, Order, StdError};
 use cw_storage_plus::Map;
 
-use crate::state::{TMP_VAULT_ASSET, VAULTS};
 use terraswap::asset::AssetInfo;
+
+use crate::state::{TMP_VAULT_ASSET, VAULTS};
 
 /// Migrates the VAULTS state from v1.0.9 and lower to v1.1.0, which includes the asset info.
 pub fn migrate_to_v110(deps: DepsMut) -> Result<(), StdError> {
@@ -14,22 +15,19 @@ pub fn migrate_to_v110(deps: DepsMut) -> Result<(), StdError> {
         .take(10)
         .collect::<Result<Vec<_>, _>>()?;
 
-    let _ = vaults_v109.iter().map(|item| {
-        let (key, vault_addr) = item;
+    vaults_v109
+        .into_iter()
+        .try_for_each(|(key, vault_addr)| -> Result<(), StdError> {
+            // All the vaults created so far are using native tokens
+            // try to parse reference as a denom value
+            let asset_info = AssetInfo::NativeToken {
+                denom: String::from_utf8(key.clone())?,
+            };
 
-        // All the vaults created so far are using native tokens
-        // try to parse reference as a denom value
-        let asset_info = AssetInfo::NativeToken {
-            denom: String::from_utf8(key.to_vec())?,
-        };
+            VAULTS.save(deps.storage, &key, &(vault_addr, asset_info))?;
 
-        VAULTS.save(
-            deps.storage,
-            &key.clone(),
-            &(vault_addr.clone(), asset_info),
-        )?;
-        Ok::<(), StdError>(())
-    });
+            Ok(())
+        })?;
 
     TMP_VAULT_ASSET.remove(deps.storage);
 
