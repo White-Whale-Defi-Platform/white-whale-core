4b09fafdb34db6f1e16351494b075d9fd58c1522
kaimen-sano <kaimen_sano@protonmail.com>
2023-02-08 22:36:54 +1300

    test(stableswap): add 18,18 test
    Adds a test case for swapping on a pool with 1m liquidity of each asset
    that has a precision of 18 decimal places. This is likely to be the case
    on chains such as Injective.

--- contracts/liquidity_hub/pool-network/terraswap_pair/src/tests/stableswap.rs
+++ contracts/liquidity_hub/pool-network/terraswap_pair/src/tests/stableswap.rs
@@ -203,4 +203,84 @@ mod tests {
             }
         );
     }
+
+    #[test]
+    #[allow(clippy::inconsistent_digit_grouping)]
+    fn does_stableswap_with_18_18_precision() {
+        let mut asset_pool_amount = Uint128::from(1000000_000000000000000000u128);
+        let mut collateral_pool_amount = Uint128::from(1000000_000000000000000000u128);
+        let mut offer_amount = Uint128::from(1000_000000000000000000u128);
+
+        let swap_result = compute_swap(
+            collateral_pool_amount,
+            asset_pool_amount,
+            offer_amount,
+            PoolFee {
+                protocol_fee: Fee {
+                    share: Decimal::from_ratio(1u128, 400u128),
+                },
+                swap_fee: Fee {
+                    share: Decimal::from_ratio(1u128, 400u128),
+                },
+                burn_fee: Fee {
+                    share: Decimal::zero(),
+                },
+            },
+            &PairType::StableSwap { amp: 50 },
+            18,
+            18,
+        )
+        .unwrap();
+        assert_eq!(
+            swap_result,
+            SwapComputation {
+                protocol_fee_amount: Uint128::new(2_499950981306193408),
+                swap_fee_amount: Uint128::new(2_499950981306193408),
+                return_amount: Uint128::new(994_980490559864976477),
+                spread_amount: Uint128::new(19607477522636707),
+                burn_fee_amount: Uint128::zero()
+            }
+        );
+
+        // apply the changes of the swap
+        asset_pool_amount -= swap_result.return_amount
+            + swap_result.spread_amount
+            + swap_result.protocol_fee_amount
+            + swap_result.swap_fee_amount
+            + swap_result.burn_fee_amount;
+        collateral_pool_amount += offer_amount;
+
+        // do a new offer, in the opposite direction of 5k this time
+        offer_amount = Uint128::new(5000_000000000000000000u128);
+        let swap_result = compute_swap(
+            asset_pool_amount,
+            collateral_pool_amount,
+            offer_amount,
+            PoolFee {
+                protocol_fee: Fee {
+                    share: Decimal::from_ratio(1u128, 400u128),
+                },
+                swap_fee: Fee {
+                    share: Decimal::from_ratio(1u128, 400u128),
+                },
+                burn_fee: Fee {
+                    share: Decimal::zero(),
+                },
+            },
+            &PairType::StableSwap { amp: 50 },
+            18,
+            18,
+        )
+        .unwrap();
+        assert_eq!(
+            swap_result,
+            SwapComputation {
+                protocol_fee_amount: Uint128::new(12_499264751528814624),
+                swap_fee_amount: Uint128::new(12_499264751528814624),
+                return_amount: Uint128::new(4974_707371108468220566),
+                spread_amount: Uint128::new(294099388474150186),
+                burn_fee_amount: Uint128::zero()
+            }
+        );
+    }
 }
