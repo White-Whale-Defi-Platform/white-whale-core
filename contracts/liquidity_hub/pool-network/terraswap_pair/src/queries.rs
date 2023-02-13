irulast/white-whale-migaloo-core

feat: StableSwap

contracts/liquidity_hub/pool-network/terraswap_pair/src/queries.rs
@@ -1,6 +1,8 @@
-use cosmwasm_std::{Deps, StdResult, Uint128};
+use std::cmp::Ordering;
 
-use terraswap::asset::{Asset, PairInfo, PairInfoRaw};
+use cosmwasm_std::{Decimal256, Deps, Fraction, StdResult, Uint128};
+
+use terraswap::asset::{Asset, PairInfo, PairInfoRaw, PairType};
 use terraswap::pair::{
     ConfigResponse, PoolResponse, ProtocolFeesResponse, ReverseSimulationResponse,
     SimulationResponse,
@@ -8,8 +10,9 @@ use terraswap::pair::{
 use terraswap::querier::query_token_info;
 
 use crate::error::ContractError;
-use crate::helpers;
-use crate::helpers::get_protocol_fee_for_asset;
+use crate::helpers::{self, calculate_stableswap_y};
+use crate::helpers::{get_protocol_fee_for_asset, StableSwapDirection};
+use crate::math::Decimal256Helper;
 use crate::state::{
     get_protocol_fees_for_asset, ALL_TIME_COLLECTED_PROTOCOL_FEES, COLLECTED_PROTOCOL_FEES, CONFIG,
     PAIR_INFO,
@@ -63,12 +66,16 @@ pub fn query_simulation(
     deps: Deps,
     offer_asset: Asset,
 ) -> Result<SimulationResponse, ContractError> {
-    let pair_info: PairInfoRaw = PAIR_INFO.load(deps.storage)?;
+    let pair_info = PAIR_INFO.load(deps.storage)?;
 
     let contract_addr = deps.api.addr_humanize(&pair_info.contract_addr)?;
 
     let offer_pool: Asset;
+    let offer_decimal;
+
     let ask_pool: Asset;
+    let ask_decimal;
+
     let collected_protocol_fees = COLLECTED_PROTOCOL_FEES.load(deps.storage)?;
 
     // To calculate pool amounts properly we should subtract the protocol fees from the pool
@@ -87,10 +94,16 @@ pub fn query_simulation(
 
     if offer_asset.info.equal(&pools[0].info) {
         offer_pool = pools[0].clone();
+        offer_decimal = pair_info.asset_decimals[0];
+
         ask_pool = pools[1].clone();
+        ask_decimal = pair_info.asset_decimals[1];
     } else if offer_asset.info.equal(&pools[1].info) {
         offer_pool = pools[1].clone();
+        offer_decimal = pair_info.asset_decimals[1];
+
         ask_pool = pools[0].clone();
+        ask_decimal = pair_info.asset_decimals[0];
     } else {
         return Err(ContractError::AssetMismatch {});
     }
@@ -102,6 +115,9 @@ pub fn query_simulation(
         ask_pool.amount,
         offer_asset.amount,
         pool_fees,
+        &pair_info.pair_type,
+        offer_decimal,
+        ask_decimal,
     )?;
 
     Ok(SimulationResponse {
@@ -140,30 +156,98 @@ pub fn query_reverse_simulation(
 
     let offer_pool: Asset;
     let ask_pool: Asset;
+    let offer_decimal;
+    let ask_decimal;
+
     if ask_asset.info.equal(&pools[0].info) {
         ask_pool = pools[0].clone();
+        ask_decimal = pair_info.asset_decimals[0];
+
         offer_pool = pools[1].clone();
+        offer_decimal = pair_info.asset_decimals[1];
     } else if ask_asset.info.equal(&pools[1].info) {
         ask_pool = pools[1].clone();
+        ask_decimal = pair_info.asset_decimals[1];
+
         offer_pool = pools[0].clone();
+        offer_decimal = pair_info.asset_decimals[0];
     } else {
         return Err(ContractError::AssetMismatch {});
     }
 
     let pool_fees = CONFIG.load(deps.storage)?.pool_fees;
-    let offer_amount_computation = helpers::compute_offer_amount(
-        offer_pool.amount,
-        ask_pool.amount,
-        ask_asset.amount,
-        pool_fees,
-    )?;
 
-    Ok(ReverseSimulationResponse {
-        offer_amount: offer_amount_computation.offer_amount,
-        spread_amount: offer_amount_computation.spread_amount,
-        swap_fee_amount: offer_amount_computation.swap_fee_amount,
-        protocol_fee_amount: offer_amount_computation.protocol_fee_amount,
-    })
+    match pair_info.pair_type {
+        PairType::ConstantProduct => {
+            let offer_amount_computation = helpers::compute_offer_amount(
+                offer_pool.amount,
+                ask_pool.amount,
+                ask_asset.amount,
+                pool_fees,
+            )?;
+
+            Ok(ReverseSimulationResponse {
+                offer_amount: offer_amount_computation.offer_amount,
+                spread_amount: offer_amount_computation.spread_amount,
+                swap_fee_amount: offer_amount_computation.swap_fee_amount,
+                protocol_fee_amount: offer_amount_computation.protocol_fee_amount,
+            })
+        }
+        PairType::StableSwap { amp } => {
+            let offer_pool = Decimal256::decimal_with_precision(offer_pool.amount, offer_decimal)?;
+            let ask_pool = Decimal256::decimal_with_precision(ask_pool.amount, ask_decimal)?;
+
+            let before_fees = (Decimal256::one()
+                .checked_sub(pool_fees.protocol_fee.to_decimal_256())?
+                .checked_sub(pool_fees.swap_fee.to_decimal_256())?)
+            .inv()
+            .unwrap_or_else(Decimal256::one)
+            .checked_mul(Decimal256::decimal_with_precision(
+                ask_asset.amount,
+                ask_decimal,
+            )?)?;
+            let before_fees_offer = before_fees.to_uint256_with_precision(offer_decimal.into())?;
+            let before_fees_ask = before_fees.to_uint256_with_precision(ask_decimal.into())?;
+
+            let new_offer_pool_amount = calculate_stableswap_y(
+                offer_pool,
+                ask_pool,
+                before_fees,
+                &amp,
+                offer_decimal,
+                ask_decimal,
+                StableSwapDirection::ReverseSimulate,
+            )?;
+
+            let max_precision = offer_decimal.max(ask_decimal);
+
+            let offer_amount = new_offer_pool_amount.checked_sub(Uint128::try_from(
+                offer_pool.to_uint256_with_precision(u32::from(max_precision))?,
+            )?)?;
+
+            // convert into the original offer precision
+            let offer_amount = match max_precision.cmp(&offer_decimal) {
+                Ordering::Equal => offer_amount,
+                Ordering::Less => offer_amount.checked_mul(Uint128::new(
+                    10u128.pow((offer_decimal - max_precision).into()),
+                ))?,
+                Ordering::Greater => offer_amount.checked_div(Uint128::new(
+                    10u128.pow((max_precision - offer_decimal).into()),
+                ))?,
+            };
+
+            let spread_amount = offer_amount.saturating_sub(Uint128::try_from(before_fees_offer)?);
+            let swap_fee_amount = pool_fees.swap_fee.compute(before_fees_ask);
+            let protocol_fee_amount = pool_fees.protocol_fee.compute(before_fees_ask);
+
+            Ok(ReverseSimulationResponse {
+                offer_amount,
+                spread_amount,
+                swap_fee_amount: swap_fee_amount.try_into()?,
+                protocol_fee_amount: protocol_fee_amount.try_into()?,
+            })
+        }
+    }
 }
 
 /// Queries the [Config], which contains the owner, pool_fees and feature_toggle

White-Whale-Defi-Platform/white-whale-core

contracts/liquidity_hub/pool-network/terraswap_pair/src/queries.rs
@@ -1,7 +1,9 @@
-use cosmwasm_std::{Deps, StdResult, Uint128};
+use std::cmp::Ordering;
+
+use cosmwasm_std::{Decimal256, Deps, Fraction, StdResult, Uint128};
 use cw_storage_plus::Item;
 
-use terraswap::asset::{Asset, PairInfo, PairInfoRaw};
+use terraswap::asset::{Asset, PairInfo, PairInfoRaw, PairType};
 use terraswap::pair::{
     ConfigResponse, PoolResponse, ProtocolFeesResponse, ReverseSimulationResponse,
     SimulationResponse,
@@ -9,8 +11,10 @@ use terraswap::pair::{
 use terraswap::querier::query_token_info;
 
 use crate::error::ContractError;
-use crate::helpers;
-use crate::helpers::get_protocol_fee_for_asset;
+use crate::helpers::{
+    self, calculate_stableswap_y, get_protocol_fee_for_asset, StableSwapDirection,
+};
+use crate::math::Decimal256Helper;
 use crate::state::{get_fees_for_asset, COLLECTED_PROTOCOL_FEES, CONFIG, PAIR_INFO};
 
 /// Queries the [PairInfo] of the pool
@@ -61,12 +65,16 @@ pub fn query_simulation(
     deps: Deps,
     offer_asset: Asset,
 ) -> Result<SimulationResponse, ContractError> {
-    let pair_info: PairInfoRaw = PAIR_INFO.load(deps.storage)?;
+    let pair_info = PAIR_INFO.load(deps.storage)?;
 
     let contract_addr = deps.api.addr_humanize(&pair_info.contract_addr)?;
 
     let offer_pool: Asset;
+    let offer_decimal;
+
     let ask_pool: Asset;
+    let ask_decimal;
+
     let collected_protocol_fees = COLLECTED_PROTOCOL_FEES.load(deps.storage)?;
 
     // To calculate pool amounts properly we should subtract the protocol fees from the pool
@@ -85,10 +93,16 @@ pub fn query_simulation(
 
     if offer_asset.info.equal(&pools[0].info) {
         offer_pool = pools[0].clone();
+        offer_decimal = pair_info.asset_decimals[0];
+
         ask_pool = pools[1].clone();
+        ask_decimal = pair_info.asset_decimals[1];
     } else if offer_asset.info.equal(&pools[1].info) {
         offer_pool = pools[1].clone();
+        offer_decimal = pair_info.asset_decimals[1];
+
         ask_pool = pools[0].clone();
+        ask_decimal = pair_info.asset_decimals[0];
     } else {
         return Err(ContractError::AssetMismatch {});
     }
@@ -100,6 +114,9 @@ pub fn query_simulation(
         ask_pool.amount,
         offer_asset.amount,
         pool_fees,
+        &pair_info.pair_type,
+        offer_decimal,
+        ask_decimal,
     )?;
 
     Ok(SimulationResponse {
@@ -139,31 +156,102 @@ pub fn query_reverse_simulation(
 
     let offer_pool: Asset;
     let ask_pool: Asset;
+    let offer_decimal;
+    let ask_decimal;
+
     if ask_asset.info.equal(&pools[0].info) {
         ask_pool = pools[0].clone();
+        ask_decimal = pair_info.asset_decimals[0];
+
         offer_pool = pools[1].clone();
+        offer_decimal = pair_info.asset_decimals[1];
     } else if ask_asset.info.equal(&pools[1].info) {
         ask_pool = pools[1].clone();
+        ask_decimal = pair_info.asset_decimals[1];
+
         offer_pool = pools[0].clone();
+        offer_decimal = pair_info.asset_decimals[0];
     } else {
         return Err(ContractError::AssetMismatch {});
     }
 
     let pool_fees = CONFIG.load(deps.storage)?.pool_fees;
-    let offer_amount_computation = helpers::compute_offer_amount(
-        offer_pool.amount,
-        ask_pool.amount,
-        ask_asset.amount,
-        pool_fees,
-    )?;
 
-    Ok(ReverseSimulationResponse {
-        offer_amount: offer_amount_computation.offer_amount,
-        spread_amount: offer_amount_computation.spread_amount,
-        swap_fee_amount: offer_amount_computation.swap_fee_amount,
-        protocol_fee_amount: offer_amount_computation.protocol_fee_amount,
-        burn_fee_amount: offer_amount_computation.burn_fee_amount,
-    })
+    match pair_info.pair_type {
+        PairType::ConstantProduct => {
+            let offer_amount_computation = helpers::compute_offer_amount(
+                offer_pool.amount,
+                ask_pool.amount,
+                ask_asset.amount,
+                pool_fees,
+            )?;
+
+            Ok(ReverseSimulationResponse {
+                offer_amount: offer_amount_computation.offer_amount,
+                spread_amount: offer_amount_computation.spread_amount,
+                swap_fee_amount: offer_amount_computation.swap_fee_amount,
+                protocol_fee_amount: offer_amount_computation.protocol_fee_amount,
+                burn_fee_amount: offer_amount_computation.burn_fee_amount,
+            })
+        }
+        PairType::StableSwap { amp } => {
+            let offer_pool = Decimal256::decimal_with_precision(offer_pool.amount, offer_decimal)?;
+            let ask_pool = Decimal256::decimal_with_precision(ask_pool.amount, ask_decimal)?;
+
+            let before_fees = (Decimal256::one()
+                .checked_sub(pool_fees.protocol_fee.to_decimal_256())?
+                .checked_sub(pool_fees.swap_fee.to_decimal_256())?
+                .checked_sub(pool_fees.burn_fee.to_decimal_256())?)
+            .inv()
+            .unwrap_or_else(Decimal256::one)
+            .checked_mul(Decimal256::decimal_with_precision(
+                ask_asset.amount,
+                ask_decimal,
+            )?)?;
+            let before_fees_offer = before_fees.to_uint256_with_precision(offer_decimal.into())?;
+            let before_fees_ask = before_fees.to_uint256_with_precision(ask_decimal.into())?;
+
+            let new_offer_pool_amount = calculate_stableswap_y(
+                offer_pool,
+                ask_pool,
+                before_fees,
+                &amp,
+                offer_decimal,
+                ask_decimal,
+                StableSwapDirection::ReverseSimulate,
+            )?;
+
+            let max_precision = offer_decimal.max(ask_decimal);
+
+            let offer_amount = new_offer_pool_amount.checked_sub(Uint128::try_from(
+                offer_pool.to_uint256_with_precision(u32::from(max_precision))?,
+            )?)?;
+
+            // convert into the original offer precision
+            let offer_amount = match max_precision.cmp(&offer_decimal) {
+                Ordering::Equal => offer_amount,
+                Ordering::Less => offer_amount.checked_mul(Uint128::new(
+                    10u128.pow((offer_decimal - max_precision).into()),
+                ))?,
+                Ordering::Greater => offer_amount.checked_div(Uint128::new(
+                    10u128.pow((max_precision - offer_decimal).into()),
+                ))?,
+            };
+
+            let spread_amount = offer_amount.saturating_sub(Uint128::try_from(before_fees_offer)?);
+            let swap_fee_amount = pool_fees.swap_fee.compute(before_fees_ask);
+            let protocol_fee_amount = pool_fees.protocol_fee.compute(before_fees_ask);
+            let burn_fee_amount = pool_fees.burn_fee.compute(before_fees_ask);
+
+            Ok(ReverseSimulationResponse {
+                offer_amount,
+                spread_amount,
+                swap_fee_amount: swap_fee_amount.try_into()?,
+                protocol_fee_amount: protocol_fee_amount.try_into()?,
+                burn_fee_amount: burn_fee_amount.try_into()?,
+            })
+        }
+    }
 }
 
 /// Queries the [Config], which contains the owner, pool_fees and feature_toggle