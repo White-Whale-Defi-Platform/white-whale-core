a63b43fbd18736bc0280f2a5046f23edbcfc32ae
Kerber0x <kerber0x@protonmail.com>
2024-01-27 18:00:43 +0000

    chore: forward fees from first epoch to the upcoming one

--- contracts/liquidity_hub/fee_distributor/src/migrations.rs
+++ contracts/liquidity_hub/fee_distributor/src/migrations.rs
@@ -104,3 +104,37 @@ pub fn migrate_to_v091(deps: DepsMut) -> Result<Vec<CosmosMsg>, StdError> {
 
     Ok(messages)
 }
+
+#[cfg(feature = "osmosis")]
+/// Fixes the broken state for the first epoch on osmosis.
+pub fn migrate_to_v091_hotfix(deps: DepsMut) -> Result<Vec<CosmosMsg>, StdError> {
+    let claimable_epochs = get_claimable_epochs(deps.as_ref())?;
+
+    let mut faulty_epoch = claimable_epochs
+        .epochs
+        .into_iter()
+        .find(|epoch| epoch.id == Uint64::from(1u64))
+        .ok_or(StdError::generic_err("Epoch not found"))?;
+
+    let fee_collector_addr = CONFIG.load(deps.storage)?.fee_collector_addr;
+
+    // collect all available funds on faulty epochs and send them back to the fee collector, to be
+    // redistributed on the next (new) epoch
+
+    let total_fees = asset::aggregate_assets(vec![], faulty_epoch.available.clone())?;
+
+    // set the available fees of this faulty epoch to zero
+    faulty_epoch.available = vec![];
+
+    // save the faulty epoch in the state
+    EPOCHS.save(deps.storage, &faulty_epoch.id.to_be_bytes(), &faulty_epoch)?;
+
+    // create messages to send total_fees back to the fee collector
+    let mut messages = vec![];
+
+    for fee in total_fees {
+        messages.push(fee.into_msg(fee_collector_addr.clone())?);
+    }
+
+    Ok(messages)
+}
