e63fd36b05e2630a1cb7dc462f1e8aab9b863599
Kerber0x <kerber0x@protonmail.com>
2024-02-13 10:52:37 +0000

    ci: print the chain that was loaded with the load env alias

--- scripts/deployment/wallet_importer.sh
+++ scripts/deployment/wallet_importer.sh
@@ -27,6 +27,5 @@ function import_deployer_wallet() {
 		echo $mnemonic | $BINARY keys add $deployer --recover >/dev/null 2>&1
 	fi
 
-	deployer='deployer_wallet'
-	deployer_address=migaloo1dpx7ytug647wefe7ajxmg5ejt68gxcfvw35f4e
+	deployer_address=$($BINARY keys show $deployer --output json | jq -r '.address')
 }
