d1a8b1918687de85767953a5ed676720a48dcd59
kaimen-sano <kaimen_sano@protonmail.com>
2024-03-25 21:17:13 +1300

    fix(testing): reorganize testing utilities
    Moves the Osmosis mocking to the `white-whale-testing` package, rather
    than `white-whale-std` package. This lets us drop the `#[cfg(test)]`
    qualifiers which restrict integration tests from consuming our package.
    
    This is a suitable restriction to drop, since it is likely that any code
    that is using `white-whale-testing` is performing testing operations,
    while our `white-whale-std` was public facing.

--- packages/white-whale-testing/src/lib.rs
+++ packages/white-whale-testing/src/lib.rs
@@ -1,2 +1,9 @@
 #[cfg(not(target_arch = "wasm32"))]
 pub mod integration;
+
+#[cfg(any(
+    feature = "token_factory",
+    feature = "osmosis_token_factory",
+    feature = "injective"
+))]
+pub mod multi_test;
